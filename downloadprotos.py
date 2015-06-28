5187cb4fbe1baca52a7879381516b63bd4401b4e
milia <mlliarm@yahoo.com>
2015-06-29 02:36:37 +0300

    Adding scraper and README

---
+++ downloadprotos.py
@@ -0,0 +1,109 @@
+__author__ = "milia"
+__email__ = "milia@protonmail.com"
+__licence__ = "GPL v.2"
+
+import datetime
+import wget
+import os
+from datetime import date
+import calendar
+import urllib2
+import time
+
+# Papers from frontpagesgr website
+def frmfrontpagesgr(efimerida, today, year):
+	try:
+		url = 'http://www.frontpages.gr/data/' + year + '/' + today + '/' + \
+		efimerida +'.jpg'
+		urllib2.urlopen(url)
+		wget.download(url)
+		oldname = efimerida + '.jpg'
+		newname = today + "_" + "FrontpagesGR" + "_" + oldname
+		os.rename(oldname, newname)
+	except urllib2.HTTPError, e:
+		#print(e.code)
+		print "ERROR 404: Paper not found !"
+		print " "
+	except urllib2.HTTPError, e:
+		print(e.args)
+
+def insertdate():
+	global today, year
+	# Insert the date from the keyboard
+	mydate = raw_input("Please insert the date in the format YYYY/MM/DD (from the year 2010 till today): ")
+	year = mydate[0:4]
+	month = mydate[5:7]
+	day = mydate[8:10]
+	today = year + month + day		
+	# Findng out the weekday
+	mydate = date(int(year), int(month), int(day))
+	weekday = calendar.day_name[mydate.weekday()]
+	print "Will download the papers of " + weekday + " " + year + "/" + month + "/" + day + " !"
+	print " "
+
+def autodate():
+	global today, year
+	# Calculating today's date, year and day of week, automatically (default)
+	mydate = time.strftime("%Y:%m:%d")
+	year = mydate[0:4]
+	month = mydate[5:7]
+	day = mydate[8:10]
+	today = year + month + day
+	weekday = time.strftime("%a") + "day"
+	print "Will download today's (" + weekday + ") papers !"
+	print " "
+
+# def datesbetween():
+# 	date1 = raw_input("Give the first date in YYYY/MM/DD format: ")
+# 	date2 = raw_input("Give the second date in YYYY/MM/DD format: ")
+# 	paper = raw_input("Give the name of the newspaper: ")
+
+# Main function
+def main():
+	# Main papers
+	efimerides_vdomadas = ['HKathimeriniI', 'TaNeaI','EthnosI', 'EfSynI', 'EstiaI','AugiI','EleutherosTyposI','RizospastisI','EspressoI',\
+					'AgoraI', 'AggelioforosI','MakedoniaI', 'TiposThessalonikisI','KontraNewsI', 'ToParonI','FreeSundayI','ToXwniI','PressTimeI', 'KarfiI',\
+					'LogosI','StarPressI', 'EloraI','ParaskinioI','ArthroI','GPrinI','KarfitsaI','DimokratiaI','ApopsiI','HNautemporikiI','HmerhsiaI','GEpoxiI',\
+					'XrhmatisthrioI','MetoxosI','DimoprasionI','DealNewsI','IhodimoprasionI','OikonomikiBEI','DimoprasiakiAI']
+
+	# Local papers of the week
+	topikes_efimerides = ['HAgonI','HNeoiAgonesI','HProinosLogosI','HAnexartitosI','TTaxydromosI','TEleutheriaI','TMagnisiaI','TProinosTyposI', \
+					'TThessaliaI','NADimokratikiRodouI','NAKoiniGnomiI','NAKykladikiI','BAPolitisI','MTAgonasI','MTXronosI','MTNeaEgnatiaI',\
+					'MTProinosTyposI','MTEleutheroVimaI','MTNeaKastoriaI','MTSimeriniSerI','MTHmerhsiaVI','PanelliniaBEI','KMParatiritsI',\
+					'KMPoliteiaI','KPatrisI','KRethemniotikaI','KNeaKritiI','KAnatolhI','IEnimerosiI','IHmeraI','IHmerhsiosI','PPeloponnisosI' \
+					,'PPatrisI','PGnomiI','PProtiI','DESyneidisiI','PAllagiI','PGegonotaI','PEleutheriaI','PProiniI','SLamiakosI','ASyneidhshI','AAttikoBhmaI',\
+					'AXtyposI','EthnikosKirikasI']
+
+    # Clear screen
+	os.system('clear')
+
+	# Welcome Message
+	print "WELCOME TO THE PROTOSELIDA (FIRST PAGES) DOWNLOADER !"
+	print "-----------------------------------------------------"
+	print " "
+
+	# Decision making follows
+	answer = raw_input("1. Would you like to enter an older date? (Y/N): ")
+	if answer == "Y" or answer == "y":
+		insertdate()
+	else:
+		autodate()
+
+	# Choose if you want the local papers as well (43)
+	topikes = raw_input("2. Do you want the local newspapers of the day as well? (Y/N): ")
+	
+	#	Call the downloading function
+	for efimerida in efimerides_vdomadas:
+		frmfrontpagesgr(efimerida,today,year)
+	if topikes == "Y" or topikes == "y":
+		for efimerida in topikes_efimerides:
+			frmfrontpagesgr(efimerida,today,year)
+	print " "
+
+	# Another option
+	# seperate_paper = raw_input("3. Would you like to download all the copies of a specific paper between two dates? (Y/N)")
+	# if seperate_paper == "Y":
+	# 	datesbetween()
+
+if __name__ == "__main__":
+	main()
